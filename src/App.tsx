7cded68524b4b8ce04273f77240683b38881c1a5
cleandevcode <52365167+cleandevcode@users.noreply.github.com>
2022-10-08 00:39:42 +0800

    Footer

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
 import { BrowserRouter, Routes, Route, Navigate, Link } from "react-router-dom";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </ChakraProvider>
   );
